4e45df62d8bf11e48c8e5994634e3f7046a622f2
farreldev <rickyalext@gmail.com>
2023-04-19 01:05:02 +0700

    update tuples Array of String

---
+++ index.ts
@@ -0,0 +1,17 @@
+// Array of number
+
+// Type inference
+let list1 = [1, 2, 3, 4];
+
+// Square bracket
+let list1_2: number[];
+list1_2 = [1, 2, 3, 4];
+
+// Generic array
+let list1_3: Array<number>;
+list1_3 = [1, 2, 3, 4];
+
+// Array of string
+// Generic
+let list2: Array<string>;
+list2 = ['a', 'b', 'c'];
