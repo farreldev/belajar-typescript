75d011625c6ecaace7cb82e236803248165bfd65
farreldev <rickyalext@gmail.com>
2023-04-25 16:31:28 +0700

    function with callback

--- index.ts
+++ index.ts
@@ -11,3 +11,15 @@ console.log(add(2, 4));
 const addEx = (a: number, b: number): number => a + b;
 
 console.log(addEx(2, 1));
+
+// function with callback
+
+type readUrl = (message: string) => void;
+
+function request(url: string, cb: readUrl) {
+  cb(url);
+}
+
+let fn = (str: string) => console.log(str);
+
+request('https://www.youtube.com', fn);
