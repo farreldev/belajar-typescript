541f86b25a607e0c998666552dff53ff0e133257
farreldev <rickyalext@gmail.com>
2023-05-17 01:23:59 +0700

    interface merge

--- index.ts
+++ index.ts
@@ -8,3 +8,20 @@ type TUser = {
   name: string;
   age: number;
 };
+
+// Merge
+// interface if define with same name it will automaticaly merge
+interface Song {
+  songName: string;
+}
+
+interface Song {
+  artisName: string;
+}
+
+let mySongList: Song = {
+  artisName: 'Ello',
+  songName: 'Masih Ada',
+};
+
+console.log(mySongList);
